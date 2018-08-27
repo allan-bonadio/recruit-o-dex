01f9fc77c2e6b49e8390621797ff98a334352014
allan-bonadio <orgmgr@tactileint.org>
2018-08-26 18:13:08 -0700

    added getAll() test for Model

--- src/Model.test.js
+++ src/Model.test.js
@@ -2,22 +2,25 @@ import 'raf/polyfill';  // says to
 import 'core-js/es6/map';
 import 'core-js/es6/set';
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
-
-import {configure, shallow, mount, render} from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+////import React from 'react';
+////import ReactDOM from 'react-dom';
+////import {Provider} from 'react-redux';
 
 import {rxStore} from './reducer';
-import {Model} from './Model';
+import {Model, getAll, putOne, postOne, getBySerial} from './Model';
 
-
-configure({ adapter: new Adapter() });
-
-describe('<Model', () => {
+describe('Model ', () => {
 	it('renders without crashing', () => {
 		const div = document.createElement('div');
+////		ReactDOM.render(<Provider store={rxStore}><Model /></Provider>, div);
+	});
+
+	it('can getAll()', (done) => {
+		const all = getAll((errors, recs) => {
+			// lskdjf
+			expect(errors).toBeFalsy();
+			expect(recs.constructor).toBe(Array);
+		});
 ////		ReactDOM.render(<Provider store={rxStore}><Model /></Provider>, div);
 	});
 });
